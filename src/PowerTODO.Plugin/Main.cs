86ba39dd51c6a743cc42ad7f098bf30bf0a26a4f
Shiro_Wei <2313247167@qq.com>
2026-04-22 01:36:09 +0800

    feat: 初始化 PowerTODO 插件项目框架
    - 实现 IPlugin 接口
    - 实现 Query 方法返回静态数据
    - 插件已能被 PowerToys Run 正常识别

---
+++ src/PowerTODO.Plugin/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Wox.Plugin;
+
+public class Main : IPlugin
+{
+    public static string PluginID => "TodoPlugin";
+    public string Name => "PowerTODO";
+    public string Description => "A TODO manager for Powertoy Run";
+    public void Init(PluginInitContext context) { }
+
+    public List<Result> Query(Query query)
+    {
+        return new List<Result>
+        {
+            new Result
+            {
+                Title = "Hello PowerTODO",
+                SubTitle = "Plugin works",
+                Action = e => true
+            }
+        };
+    }
+}
