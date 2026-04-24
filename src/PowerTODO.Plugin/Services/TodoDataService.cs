c2b49ac4d63cc6fc7e5936e62bcb7d68177e21e1
Shiro_Wei <2313247167@qq.com>
2026-04-24 15:30:06 +0800

    feat(TodoDataService): 实现文件的读取与保存
    - 实现 LoadTodos 和 SaveTodos 方法
    - 将 _todos 标记为可空对象，由 LoadTodos 方法确保其不为空

--- src/PowerTODO.Plugin/Services/TodoDataService.cs
+++ src/PowerTODO.Plugin/Services/TodoDataService.cs
@@ -2,21 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 public class TodoDataService : ITodoDataService
 {
     // 类的私有只读成员变量 依赖注入
     private readonly string _dataFilePath;
-    // 读取后的 todo 列表存在变量
-    private List<TodoItem> _todos;
+    // 读取 todo 列表存储为变量
+    private List<TodoItem>? _todos;
 
     public TodoDataService(string dataDirectory)
     {
+        // Path.Combine 实现跨平台兼容性
         _dataFilePath = Path.Combine(dataDirectory, "todo.json");
+        LoadTodos();
     }
 
-    private void LoadTodos(){}
-    private void SaveTodos(){}
+    private void LoadTodos()
+    {
+        if (File.Exists(_dataFilePath))
+        {
+            var json = File.ReadAllText(_dataFilePath);
+            // 序列化后结果可能为 null 使用空合并运算符（??）保证后续操作合法
+            _todos = JsonSerializer.Deserialize<List<TodoItem>>(json) ?? new List<TodoItem>();
+        } 
+        else
+        {
+            _todos = new List<TodoItem>();
+        }
+    }
+    private void SaveTodos()
+    {
+        var json = JsonSerializer.Serialize(_todos);
+        File.WriteAllText(_dataFilePath, json);
+    }
 
     public List<TodoItem> List(string? keyword = null)
     {
